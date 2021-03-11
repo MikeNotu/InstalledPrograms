d6f5cfea6dbaa6399baa93d1ec30f12590247869
MikeNotu <mikebateman98@gmail.com>
2021-03-11 06:36:43 -0600

    An Old program that I did for a class.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace getInstalledApplications
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lView_SoftwareOverview = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lView_SoftwareOverview
+            // 
+            this.lView_SoftwareOverview.Location = new System.Drawing.Point(12, 12);
+            this.lView_SoftwareOverview.Name = "lView_SoftwareOverview";
+            this.lView_SoftwareOverview.Size = new System.Drawing.Size(1065, 444);
+            this.lView_SoftwareOverview.TabIndex = 0;
+            this.lView_SoftwareOverview.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(776, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generar PDF";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1089, 526);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lView_SoftwareOverview);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lView_SoftwareOverview;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+
+namespace getInstalledApplications
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<string> Software = getInstalledSoftware();
+
+            foreach(string s in Software)
+            {
+                ListViewItem lvItem = new ListViewItem() { Text = s };
+                lView_SoftwareOverview.Items.Add(lvItem);
+            }
+        }
+
+        private List<string> getInstalledSoftware()
+        {
+            List<string> installedSoftware = new List<string>();
+
+            ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Product");
+
+            ManagementObjectCollection result = mSearcher.Get();
+
+            foreach(ManagementObject mObject in result)
+            {
+                if (mObject["Name"] != null)
+                    installedSoftware.Add(mObject["Name"].ToString());
+            }
+
+            return installedSoftware;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
